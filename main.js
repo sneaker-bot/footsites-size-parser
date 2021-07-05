e7bb6c7ed966e9984d35b1390a8a94cbc18fe95f
backenddev <74377541+jsdeobfuscator@users.noreply.github.com>
2021-07-05 17:27:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,27 @@ async function parse(body, size){
 			data = JSON.parse(data);
 			data = data['pdp']['sellableUnits']; 
 
+			var available_sizes = [];
+
+			data.map(b => { 
+
+				available_sizes.push(b['attributes'][0]['value']['_text']);
+
+			});
+
+			available_sizes = new Set(available_sizes);
+			available_sizes = Array.from(available_sizes);
+
+			function random(min, max){
+
+    			return Math.floor(Math.random() * (max - min) + min);
+			};
+
+			if(size == 'random'){
+
+				size = available_sizes[random(0, available_sizes.length - 1)];
+			};
+
 			var size_id = data.find(b => b['attributes'][0]['value']['_text'] == size);
 
 			size_id = size_id['attributes'][0]['id']['_text'];
